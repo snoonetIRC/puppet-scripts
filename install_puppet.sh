a543ba1b22ba4bdee25456dd3c83a88fec3deefa
Evan Snoonet <evan@snoonet.org>
2016-06-29 13:47:27 -0400

    Initial commit

---
+++ install_puppet.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+service puppet stop
+apt-get remove puppet-agent
+rm -rf /opt/puppetlabs
+rm -rf /etc/puppetlabs
+rm -rf /var/run/puppetlabs
+codename=`lsb_release --codename | cut -f2`
+wget https://apt.puppetlabs.com/puppetlabs-release-pc1-$codename.deb
+dpkg -i puppetlabs-release-pc1-$codename.deb
+rm puppetlabs-release-pc1-$codename.deb
+apt-get update
+apt-get install puppet-agent
+echo "[main]" >> /etc/puppetlabs/puppet/puppet.conf
+echo "server = puppet.snoonet.org" >> /etc/puppetlabs/puppet/puppet.conf
+echo "environment = production" >> /etc/puppetlabs/puppet/puppet.conf
+echo "runinterval = 20m" >> /etc/puppetlabs/puppet/puppet.conf
+/opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
